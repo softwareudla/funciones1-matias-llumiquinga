b6a089b097a27cf89c73e20e9c545e6cf77dc482
matias.llumiquinga <matias.llumiquinga@udla.edu.ec>
2024-10-20 11:06:26 -0500

    Eliminación de líneas de código vacías

--- funciones.h
+++ funciones.h
@@ -5,7 +5,6 @@ int ingresarCantidadProductos();
 void ingresarNombresProductos(char nombreProducto[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES], int i);
 void ingresarPreciosProductos(float preciosProductos[CANTIDAD_PRODUCTOS], int i, char nombreProducto[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES]);
 
-
 float obtenerPrecioTotal(float precioTotal, float preciosProductos[CANTIDAD_PRODUCTOS], int i);
 float obtenerPrecioMin(float min, float preciosProductos[CANTIDAD_PRODUCTOS], int i);
 float obtenerPrecioMax(float max, float preciosProductos[CANTIDAD_PRODUCTOS], int i);
@@ -22,6 +21,5 @@ void imprimirCalculos(  float precioTotal, float min, float max, float promedio,
                         int cantidadProductos, 
                         char nombreProducto[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES], 
                         float preciosProductos[CANTIDAD_PRODUCTOS]);
-
 
 void imprimirSeparadores();
--- main.c
+++ main.c
@@ -11,10 +11,8 @@ int main (int argc, char *argv[])
     char nombresProductos[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES];
     float preciosProductos[CANTIDAD_PRODUCTOS];
     float precioTotal=0, min=10000, max=0, promedio=0;
-
     int cantidadProductos=0;
 
-   
     cantidadProductos =ingresarCantidadProductos();    
 
     imprimirSeparadores();
@@ -29,28 +27,20 @@ int main (int argc, char *argv[])
     for (int i = 0; i < cantidadProductos; i++)
     {
         ingresarPreciosProductos(preciosProductos, i, nombresProductos);
-    }
 
-
-    for (int i = 0; i < cantidadProductos; i++)
-    {
         precioTotal= obtenerPrecioTotal(precioTotal,preciosProductos, i);
         min=obtenerPrecioMin(min, preciosProductos,i);
         max=obtenerPrecioMax(max, preciosProductos, i);
     }
+
     
     promedio=obtenerPromedio(precioTotal,cantidadProductos);
-
-
-    
 
     imprimirProductosPrecios(cantidadProductos, nombresProductos, preciosProductos);
 
     imprimirCalculos(precioTotal,min,max,promedio,cantidadProductos,nombresProductos, preciosProductos);
 
     buscarProducto(nombresProductos, preciosProductos, cantidadProductos);
-    
-
 
     return 0;
 }
