7e4cac3213d95346663b88480adc56a0d45fe800
matias.llumiquinga <matias.llumiquinga@udla.edu.ec>
2024-10-20 11:15:55 -0500

    //Código de prueba para ver autograding

--- funciones.h
+++ funciones.h
@@ -1,25 +1,4 @@
-#define CANTIDAD_PRODUCTOS 10
-#define CANTIDAD_CARACTERES 22
-
-int ingresarCantidadProductos();
-void ingresarNombresProductos(char nombreProducto[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES], int i);
-void ingresarPreciosProductos(float preciosProductos[CANTIDAD_PRODUCTOS], int i, char nombreProducto[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES]);
-
-float obtenerPrecioTotal(float precioTotal, float preciosProductos[CANTIDAD_PRODUCTOS], int i);
-float obtenerPrecioMin(float min, float preciosProductos[CANTIDAD_PRODUCTOS], int i);
-float obtenerPrecioMax(float max, float preciosProductos[CANTIDAD_PRODUCTOS], int i);
-float obtenerPromedio(float precioTotal, int cantiddProductos);
-
-void buscarProducto(char nombreProducto[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES], 
-                    float preciosProductos[CANTIDAD_PRODUCTOS], 
-                    int cantidadProductos);
-
-void imprimirProductosPrecios(  int cantidadProductos, char nombreProducto[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES], 
-                                float preciosProductos[CANTIDAD_PRODUCTOS]);
-
-void imprimirCalculos(  float precioTotal, float min, float max, float promedio,
-                        int cantidadProductos, 
-                        char nombreProducto[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES], 
-                        float preciosProductos[CANTIDAD_PRODUCTOS]);
-
-void imprimirSeparadores();
+void leerProducto(char nombres[10][50], float precios[10], int num, int i);
+void imprimirProducto(char nombres[10][50], float precios[10], int num, int i);
+void buscarProductoXNombre(char nombres[10][50], float precios[10], char nombreBuscar[50]);
+int MenuInicial(char nombres[10][50], float precios[10],int i);
--- main.c
+++ main.c
@@ -1,47 +1,30 @@
 #include <stdio.h>
-#include <string.h>
 #include "funciones.h"
 
-//#define CANTIDAD_PRODUCTOS 10
-//#define CANTIDAD_CARACTERES 22
+int main (int argc, char *argv[]) {
 
-
-int main (int argc, char *argv[])
-{
-    char nombresProductos[CANTIDAD_PRODUCTOS][CANTIDAD_CARACTERES];
-    float preciosProductos[CANTIDAD_PRODUCTOS];
-    float precioTotal=0, min=10000, max=0, promedio=0;
-    int cantidadProductos=0;
-
-    cantidadProductos =ingresarCantidadProductos();    
-
-    imprimirSeparadores();
-
-    for (int i = 0; i < cantidadProductos; i++)
+    char nombreProductos[10][50];
+    float precioProductos[10];
+    int opc=1;
+    int cont=0;
+    do
     {
-        ingresarNombresProductos(nombresProductos,i);
-    }
+        cont+=MenuInicial(nombreProductos,precioProductos,cont);
+        printf("Desea seleccionar otra opcion:\n1.Si\n2.No\n>>");
+        scanf("%d",&opc);
+    } while (opc==1);
     
-    imprimirSeparadores();
 
-    for (int i = 0; i < cantidadProductos; i++)
-    {
-        ingresarPreciosProductos(preciosProductos, i, nombresProductos);
+    //leerProducto(nombreProductos,precioProductos,1,0);
 
-        precioTotal= obtenerPrecioTotal(precioTotal,preciosProductos, i);
-        min=obtenerPrecioMin(min, preciosProductos,i);
-        max=obtenerPrecioMax(max, preciosProductos, i);
-    }
+    //imprimirProducto(nombreProductos,precioProductos,1,0);
 
-    
-    promedio=obtenerPromedio(precioTotal,cantidadProductos);
+    //char nombreABuscar[50]="Arroz";
 
-    imprimirProductosPrecios(cantidadProductos, nombresProductos, preciosProductos);
+    //buscarProductoXNombre(nombreProductos,precioProductos,nombreABuscar);
 
-    imprimirCalculos(precioTotal,min,max,promedio,cantidadProductos,nombresProductos, preciosProductos);
 
-    buscarProducto(nombresProductos, preciosProductos, cantidadProductos);
+
 
     return 0;
 }
-
